5d4d43ea7fd09e930bdc537a1051658ff0682220
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2023-01-07 14:14:37 +0200

    implement fileReader

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 using namespace std::chrono;
@@ -13,31 +14,26 @@ private:
     string name;
     string location;
     float price;
-    bool isPerishable;
     chrono::system_clock::time_point dueDate;
 
 public:
-    Product(string name, string location, float price, bool isPerishable, chrono::system_clock::time_point dueDate)
-            : name(name), location(location), price(price), isPerishable(isPerishable), dueDate(dueDate) {}
+    Product(string name, string location, float price, chrono::system_clock::time_point dueDate)
+            : name(name), location(location), price(price), dueDate(dueDate) {}
 
     string getName() { return name; }
     string getLocation() { return location; }
     float getPrice() { return price; }
-    bool getIsPerishable() { return isPerishable; }
     void setLocation(string newLocation) { location = newLocation; }
     chrono::system_clock::time_point getDueDate() { return dueDate; }
     void setDueDate(chrono::system_clock::time_point newDueDate) { dueDate = newDueDate; }
 };
-
 // define Shelf class
 class Shelf {
 private:
     string type;
     vector<Product> productsVec;
-
 public:
     Shelf(string type) : type(type) {}
-
     string getType() { return type; }
     vector<Product> getProducts() { return productsVec; }
     void addProduct(Product product)
@@ -56,14 +52,12 @@ public:
 class Supermarket {
 private:
     vector<Shelf> shelves;
-
 public:
     vector<Shelf> getShelves() { return shelves; }
     void addShelf(Shelf shelf)
     {
         shelves.push_back(shelf);
     }
-
     void removeShelf(Shelf shelf) {
         for (int i = 0; i < shelves.size(); i++) {
             if (shelves[i].getType() == shelf.getType()) {
@@ -76,7 +70,6 @@ public:
         for (Shelf& shelf : shelves) {
             // get the products on this shelf
             vector<Product> products = shelf.getProducts();
-
             // remove any expired products from the shelf
             for (Product& product : products) {
                 if (product.getDueDate() < chrono::system_clock::now()) {
@@ -85,7 +78,6 @@ public:
             }
         }
     }
-
     void Take(Product product, string shelfType) {
         for (Shelf& shelf : shelves) {
             if (shelf.getType() == shelfType) {
@@ -94,7 +86,6 @@ public:
             }
         }
     }
-
     void view()
     {
         bool exist = false;
@@ -109,21 +100,16 @@ public:
                 empty = false;
                 cout << "Product name: " << product.getName() << endl;
             }
-
             if (empty) cout << "Container is empty!" << endl;
             cout << "---------------------" << endl;
-
         }
-
         if (!exist) cout << "Container isn`t exist!" << endl;
     }
-
     void viewContainerType() {
         for (Shelf& shelf : shelves) {
             cout << "Container type: " << shelf.getType() << endl;
         }
     }
-
     void viewContainerType(string name)
     {
         bool exist = false;
@@ -140,13 +126,11 @@ public:
                     empty = false;
                     cout << "Product name: " << product.getName() << endl;
                 }
-
                 if (empty) cout << "Container is empty!" << endl;
                 cout << "---------------------" << endl;
             }
 
         }
-
         if (!exist) cout << "Container isn`t exist!" << endl;
     }
 
@@ -158,7 +142,6 @@ public:
                 cout << "Product Name: " << product.getName() << endl;
                 cout << "Location: " << product.getLocation() << endl;
                 cout << "Price: $" << product.getPrice() << endl;
-                cout << "Is Perishable: " << product.getIsPerishable() << endl;
                 time_t dueDate = chrono::system_clock::to_time_t(product.getDueDate());
                 tm tm;
                 localtime_s(&tm, &dueDate);
@@ -169,12 +152,9 @@ public:
             }
         }
     }
-
-
     void Move(Product product, string fromShelfType, string toShelfType) {
         // first remove the product from the "from" shelf
         Take(product, fromShelfType);
-
         // then add the product to the "to" shelf
         for (Shelf& shelf : shelves) {
             if (shelf.getType() == toShelfType) {
@@ -184,11 +164,9 @@ public:
             }
         }
     }
-
     void Add(Product product, string shelfType)
     {
         bool shelfExists = false;
-
         // check if the shelf exists
         for (Shelf& shelf : shelves) {
             if (shelf.getType() == shelfType) {
@@ -196,7 +174,6 @@ public:
                 break;
             }
         }
-
         // if the shelf doesn't exist, create it and add the product to it
         if (!shelfExists) {
             Shelf newShelf(shelfType);
@@ -213,99 +190,41 @@ public:
             }
         }
     }
-
 };
-
-
 
 int main() {
     Supermarket supermarket;
+    // open file for reading
+    ifstream file("supermarket_data.txt");
+    if (!file.is_open()) {
+        cout << "Error: unable to open file" << endl;
+        return 1;
+    }
 
-    system_clock::time_point dueDateMilk = system_clock::now() + (hours(24) * 2);
-    Product milk("Milk", "Dairy", 3.99, true, dueDateMilk);
-    supermarket.Add(milk, "Dairy");
+    // read each line from the file
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
 
-    system_clock::time_point dueDateBread = system_clock::now() + hours(24);
-    Product bread("Bread", "Bakery", 2.99, true, dueDateBread);
-    supermarket.Add(bread, "Bakery");
+        // read product data from the line
+        string name, location;
+        float price;
+        int dueDateDays;
+        iss >> name >> location >> price >> dueDateDays;
 
-    system_clock::time_point dueDateBlackBread = system_clock::now() + (hours(24) * 2);
-    Product blackBread("BlackBread", "Bakery", 2.99, true, dueDateBlackBread);
-    supermarket.Add(blackBread, "Bakery");
-    //// create some shelves
-    //Shelf dairyShelf("Dairy");
-    //Shelf produceShelf("Produce");
-    //Shelf frozenShelf("Frozen");
-    //Shelf bakeryShelf("Bakery");
+        // calculate due date as current time plus number of days
+        system_clock::time_point dueDate = system_clock::now() + (hours(24) * dueDateDays);
 
-    //// create some products
-    //system_clock::time_point dueDateMilk = system_clock::now() + (hours(24) * 2);
-    //Product milk("Milk", "Dairy", 3.99, true, dueDateMilk);
+        // create product and add it to the supermarket
+        Product product(name, location, price, dueDate);
+        supermarket.Add(product, location);
+    }
 
-    //system_clock::time_point dueDateBread = system_clock::now() + hours(24);
-    //Product bread("Bread", "Bakery", 2.99, true, dueDateBread);
-
-    //system_clock::time_point dueDateBlackBread = system_clock::now() + (hours(24) * 2);
-    //Product blackBread("BlackBread", "Bakery", 2.99, true, dueDateBlackBread);
-
-    //system_clock::time_point dueDateBananas = system_clock::now() - hours(24);
-    //Product bananas("Bananas", "Produce", 0.99, false, dueDateBananas);
-
-    //system_clock::time_point dueDateIceCream = system_clock::now() + (hours(24) * 2);
-    //Product iceCream("Ice Cream", "Frozen", 4.99, true, dueDateIceCream);
-
-    //// add products to their respective shelves
-    //dairyShelf.addProduct(milk);
-    //bakeryShelf.addProduct(bread);
-    //bakeryShelf.addProduct(blackBread);
-    //produceShelf.addProduct(bananas);
-    //frozenShelf.addProduct(iceCream);
-    //
-
-    //// add shelves to the supermarket
-    //supermarket.addShelf(dairyShelf);
-    //supermarket.addShelf(bakeryShelf);
-    //supermarket.addShelf(produceShelf);
-    //supermarket.addShelf(frozenShelf);
-
+    supermarket.view();
     supermarket.viewContainerType();
-    cout << "====================" << endl;
-
-    // iterate through all products in the supermarket
     supermarket.viewAllInfo();
 
 
-
-    // simulate the passage of time
-    cout << "One day has passed..." << endl << endl;
-    supermarket.nextDay();
-    cout << "====================" << endl;
-
-    // iterate through all products in the supermarket again
-    supermarket.viewAllInfo();
-    //cout << "remove one..." << endl << endl;
-    //supermarket.Take(milk, "Dairy");
-
-
-
-    supermarket.viewContainerType();
-
-    cout << endl;
-    supermarket.viewContainerType("Bakery");
-
-    cout << endl;
-    supermarket.view();
-
-
-    cout << "Move..." << endl;
-    //supermarket.Move(milk, "Dairy", "Bakery");
-    supermarket.view();
-
-    cout << "Add additional..." << endl;
-    system_clock::time_point dueDate = system_clock::now() + hours(24);
-    Product newProduct("New Product", "Bakery", 10.99, false, dueDate);
-    supermarket.Add(newProduct, "Bakery");
-
-    supermarket.view();
     return 0;
+
 }
